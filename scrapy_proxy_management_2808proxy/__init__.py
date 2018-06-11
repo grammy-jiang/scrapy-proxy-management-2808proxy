d1b2daa6e08e1e1f445f1fdc6cb790b1c1a2bea0
Grammy Jiang <719388+grammy-jiang@users.noreply.github.com>
2018-06-11 12:36:36 +0800

    move functions basic_auth_header and get_proxy out from class httpproxy

--- scrapy_proxy_management_2808proxy/__init__.py
+++ scrapy_proxy_management_2808proxy/__init__.py
@@ -1,4 +1,13 @@
+import base64
 from contextlib import contextmanager
+
+from scrapy.utils.python import to_bytes
+from six.moves.urllib.parse import unquote, urlunparse
+
+try:
+    from urllib2 import _parse_proxy
+except ImportError:
+    from urllib.request import _parse_proxy
 
 
 @contextmanager
@@ -9,3 +18,22 @@ def unfreeze_settings(settings):
         yield settings
     finally:
         settings.frozen = original_status
+
+
+def basic_auth_header(username, password, auth_encoding):
+    user_pass = to_bytes(
+        '{}:{}'.format(unquote(username), unquote(password)),
+        encoding=auth_encoding)
+    return base64.b64encode(user_pass).strip()
+
+
+def get_proxy(url, orig_type, auth_encoding):
+    proxy_type, user, password, host_port = _parse_proxy(url)
+    proxy_url = urlunparse((proxy_type or orig_type, host_port, '', '', '', ''))
+
+    if user:
+        creds = basic_auth_header(user, password, auth_encoding)
+    else:
+        creds = None
+
+    return creds, proxy_url
